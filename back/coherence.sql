c12e06e06b9049b600258b6c207c97907b64db45
Twaninja <50709124+Twaninja@users.noreply.github.com>
2021-12-02 16:57:02 +0100

    procedures

--- back/coherence.sql
+++ back/coherence.sql
@@ -143,6 +143,100 @@ END #
 -- END #
 
 
--- 
+-- CREATION D'UN EMPRUNT
+DROP PROCEDURE IF EXISTS creation_emprunt;
+DELIMITER #
+CREATE PROCEDURE creation_emprunt 
+    (IN id_adherent_emprunt INT,
+    IN id_velo_emprunt INT)
+BEGIN
+    DECLARE heure_depart DATETIME DEFAULT NOW();
+    DECLARE kilometrage_depart_emprunt INT DEFAULT 0;
+    DECLARE id_station_emprunt INT;
+    DECLARE nb_emprunt_en_cours INT;
+
+-- count nb_emprunt en cours avec ce vélo
+    SELECT COUNT(*) INTO nb_emprunt_en_cours FROM EMPRUNT WHERE ID_VELO = id_velo_emprunt AND HEURE_RENDU IS NULL;
+
+IF nb_emprunt_en_cours > 0 THEN
+    signal sqlstate '45000' set message_text = 'Un emprunt est déja en cours avec ce vélo, tu forces';
+END IF;
+
+-- select id_station
+    SELECT ID_STATION INTO id_station_emprunt 
+    FROM VELO WHERE ID_VELO=id_velo_emprunt;
+
+-- update le nombre de bornes dispo après emprunt
+    UPDATE STATION SET NB_BORNES_DISPO=NB_BORNES_DISPO+1 
+    WHERE STATION.ID_STATION=id_station_emprunt;
+
+-- Mettre id_station à null dans le velo
+    UPDATE VELO SET ID_STATION=NULL 
+    WHERE VELO.ID_VELO=id_velo_emprunt;
+
+    SELECT KILOMETRAGE INTO kilometrage_depart_emprunt
+    FROM VELO WHERE ID_VELO=id_velo_emprunt;
+
+    INSERT INTO `EMPRUNT` (`KILOMETRAGE_DEPART`, `HEURE_DEPART`, `ID_ADHERENT`, `ID_STATION`, `ID_VELO`)
+    VALUES (kilometrage_depart_emprunt, heure_depart, id_adherent_emprunt, id_station_emprunt, id_velo_emprunt);
+    
+END #
+
+-- rendu d'un velo après emprunt
+DROP PROCEDURE IF EXISTS rendu_emprunt;
+DELIMITER #
+CREATE PROCEDURE rendu_emprunt 
+    (IN id_station_rendu INT,
+    IN id_velo_emprunt INT)
+BEGIN
+    DECLARE heure_rendu DATETIME DEFAULT NOW();
+    DECLARE kilometrage_rendu INT DEFAULT 0;
+    DECLARE distance_station INT DEFAULT 0;
+    DECLARE nb_bornes_disponibles INT DEFAULT 0;
+    DECLARE nb_bornes_total INT DEFAULT 0;
+    DECLARE id_emprunt_a_rendre INT DEFAULT 0;
+
+-- RECUP ID_EMPRUNT
+    SELECT ID_EMPRUNT INTO id_emprunt_a_rendre FROM EMPRUNT 
+    WHERE ID_VELO=id_velo_emprunt AND HEURE_RENDU IS NULL;
+
+-- kilometrage depart
+    SELECT KILOMETRAGE_DEPART INTO kilometrage_rendu FROM EMPRUNT 
+    WHERE ID_EMPRUNT=id_emprunt_a_rendre;
+
+-- distance entre les stations
+    SELECT DISTANCE INTO distance_station FROM DISTANCE
+    WHERE ID_STATION1=id_station_rendu;
+
+-- check si assez de bornes dispo dans la station
+    SELECT NB_BORNES_DISPO INTO nb_bornes_disponibles FROM STATION
+    WHERE ID_STATION=id_station_rendu;
+
+    IF nb_bornes_disponibles > 0 THEN
+        UPDATE STATION SET NB_BORNES_DISPO=nb_bornes_disponibles-1
+        WHERE ID_STATION=id_station_rendu;
+
+        UPDATE EMPRUNT SET HEURE_RENDU=heure_rendu
+        WHERE ID_VELO=id_velo_emprunt;
+
+        -- update kilometrage et station velo
+        UPDATE VELO SET KILOMETRAGE=kilometrage_rendu+distance_station
+        WHERE ID_VELO=id_velo_emprunt;
+
+        UPDATE VELO SET ID_STATION=id_station_rendu
+        WHERE ID_VELO=id_velo_emprunt;
+
+        -- UPDATE EMPRUNT
+        UPDATE EMPRUNT SET HEURE_RENDU=heure_rendu
+        WHERE ID_EMPRUNT=id_emprunt_a_rendre;
+
+    ELSE
+        -- RAISE ERROR
+        signal sqlstate '45000' set message_text = 'Impossible de rendre dans cette station, pas assez de bornes disponnibles';
+
+    END IF;
+
+END #
+
 
 DELIMITER ;
